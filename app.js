3c116b81877723d2fdfb969764f320a00e4712dc
ArcaneDev 🔮🪄 <xunknown0@protonmail.com>
2025-11-01 00:50:08 +0800

    Add User Index

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
+
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+
 const productsRouter = require('./routes/products');
 const reviewsRouter = require('./routes/reviews');
 
@@ -23,9 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/products', productsRouter);
-app.use('/reviews', reviewsRouter);
+app.use('/products/:id/reviews', reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
